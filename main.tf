60877d832bc01cc07541f791a72cbf09a7af563c
amitgupta58 <36296144+amitgupta58@users.noreply.github.com>
2022-03-02 01:00:51 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,4 +15,5 @@ provider "aws" {
 module "aws-s3-challenge3Module" {
   source  = "app.terraform.io/amitgupta58/aws-s3-challenge3Module/provider"
   version = "1.0.0"
+  name = "bucket2delete-amit-123"
 }
